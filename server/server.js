7f50e7d4c79fc898579b2187735ff0473a3035d9
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-20 21:32:20 -0700

    creata generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express'),
       port = process.env.PORT || 3000,
       socketIO = require('socket.io'),
       http = require('http'),
+      {generateMessage} = require('./utils/message'),
       app = express();
 
 var server = http.createServer(app);
@@ -13,30 +14,18 @@ app.use(express.static(path.join(__dirname, '/../public')))
 io.on('connection', (socket) => {
   console.log('New User Connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to Chat Node',
-    createdAt: new Date().getTime()
-  });
-  
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin','Welcome to Chat Node'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
 
 
   socket.on('createMessage', function(message) {
-    // io.emit('newMessage', {
+    io.emit('newMessage', generateMessage(message.from,message.text));
+    // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
     console.log(`createMessage`, message);
   });
 
